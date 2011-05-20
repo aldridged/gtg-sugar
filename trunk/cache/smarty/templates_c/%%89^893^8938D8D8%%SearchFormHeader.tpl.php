8ed12cd2ba6a8ec92e50f318ed43288a257584be
aldridged@gmail.com <aldridged@gmail.com@ce15a44f-6aab-047a-11eb-47c6a2e4ae69>
2011-05-20 14:09:35 +0000

    Adjusted new line item start date auto populate to take into account getDate() returning 0-11 instead of 1-12

--- trunk/cache/smarty/templates_c/%%89^893^8938D8D8%%SearchFormHeader.tpl.php
+++ trunk/cache/smarty/templates_c/%%89^893^8938D8D8%%SearchFormHeader.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.11, created on 2011-04-07 13:25:24
+<?php /* Smarty version 2.6.11, created on 2011-05-19 13:30:40
          compiled from cache/modules/AOS_Products/SearchFormHeader.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('function', 'sugar_getjspath', 'cache/modules/AOS_Products/SearchFormHeader.tpl', 3, false),)), $this); ?>
