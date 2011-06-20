d41f53965a1cff52570cc3c4d45583e097ca39e3
aldridged@gmail.com <aldridged@gmail.com@ce15a44f-6aab-047a-11eb-47c6a2e4ae69>
2011-06-20 21:56:47 +0000

    Adjusted quote feed.

--- trunk/modules/AOS_Quotes/SugarFeeds/AOS_QuotesFeed.php
+++ trunk/modules/AOS_Quotes/SugarFeeds/AOS_QuotesFeed.php
@@ -45,7 +45,7 @@ class AOS_QuotesFeed extends FeedLogicBase{
     	global $locale;
         $text = '';
         if(empty($bean->fetched_row)){
-          $text =  '{SugarFeed.CREATED_QUOTE} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name.'] {SugarFeed.FOR} [Accounts:' . $bean->account_id . ':' . $bean->account_name . ']';
+          $text =  '{SugarFeed.CREATED_QUOTE} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name.']';
 	}
 		
         if(!empty($text)){ 
