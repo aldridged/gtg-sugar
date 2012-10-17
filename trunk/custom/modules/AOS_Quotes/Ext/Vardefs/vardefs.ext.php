e69e424f54979d6cc934e69e9e41dc18e3637b87
aldridged@gmail.com <aldridged@gmail.com@ce15a44f-6aab-047a-11eb-47c6a2e4ae69>
2012-10-17 19:38:15 +0000

    Modified vardefs to set a default quote template

--- trunk/custom/modules/AOS_Quotes/Ext/Vardefs/vardefs.ext.php
+++ trunk/custom/modules/AOS_Quotes/Ext/Vardefs/vardefs.ext.php
@@ -34,5 +34,6 @@ $dictionary['AOS_Quotes']['fields']['stage']['reportable']=true;
 $dictionary['AOS_Quotes']['fields']['invoice_status']['default']='mail';
 $dictionary['AOS_Quotes']['fields']['invoice_status']['reportable']=true;
 
+$dictionary['AOS_Quotes']['fields']['template_ddown_c']['default']='31d00d62-8892-7587-4153-4d9c843c2843';
  
 ?>
