58b50ea7f3f308a21360302ffee46b3296e764a6
aldridged <aldridged@gmail.com>
2014-01-19 23:13:19 +0000

    Debugged to raise case properly into production system - several return variables were different to the test system.

--- portal/processRaiseCase.php
+++ portal/processRaiseCase.php
@@ -21,14 +21,14 @@ $session = $result['id'];
 /* Find CRM Project (with related account id)*/
 $result = RestCall('get_entry_list',array('session' => $session, 'module_name' => 'Project', 'query' => 'name="'.$_GET['job'].'"', 'order_by' => '', 'offset' => 0, 'select_fields' => array('id'), 'link_name_to_fields_array' => array(array('name' => 'accounts', value => array('id'))), 'max_results' => 1, 'deleted' => 0));
 $jobid = $result['entry_list'][0]['id'];
-$acctid = $result['relationship_list'][0]['records'][0][0]['value'];
+$acctid = $result['relationship_list'][0][0]['records'][0]['id']['value'];
 
 /* Create new case */
 $result = RestCall('set_entry',array('session' => $session, 'module_name' => 'Cases', array('name' => $_GET['subject'], 'description' => $_GET['description'], 'jobnumber_c' => $_GET['job'])));
 $caseid = $result['id'];
 
 /* Link Job to Case */
-$result = RestCall('set_relationship',array('session' => $session, 'module_name' => 'Projects', 'module_id' => $jobid, 'link_field_name' => 'cases', 'related_ids' => array($caseid)));
+$result = RestCall('set_relationship',array('session' => $session, 'module_name' => 'Project', 'module_id' => $jobid, 'link_field_name' => 'cases', 'related_ids' => array($caseid)));
 
 /* Link Account to Case */
 $result = RestCall('set_relationship',array('session' => $session, 'module_name' => 'Accounts', 'module_id' => $acctid, 'link_field_name' => 'cases', 'related_ids' => array($caseid)));
