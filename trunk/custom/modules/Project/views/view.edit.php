0c2b009cce5bff1f8dd4350706de23bb51e7b8c1
aldridged@gmail.com <aldridged@gmail.com@ce15a44f-6aab-047a-11eb-47c6a2e4ae69>
2014-05-11 01:44:04 +0000

    Code to inherit specified values from Accounts to Job when creating a new job from an account

---
+++ trunk/custom/modules/Project/views/view.edit.php
@@ -0,0 +1,24 @@
+<?php
+require_once('include/MVC/View/views/view.edit.php');
+
+class ProjectViewEdit extends ViewEdit{
+
+  function ProjectViewEdit() {
+    parent::SugarView();
+  }
+
+  function display(){
+    $this->ev->process();
+    //echo "<!-- \n";
+    //var_dump($_REQUEST);
+    //echo "-->";
+    if (empty($this->ev->focus->id) && ($_REQUEST['relate_to'] == 'projects_accounts')) {
+	  $parent = new Account();
+      $parent->retrieve($_REQUEST['relate_id']);
+      if(isset($parent->specialbilling_c)) { $this->ev->fieldDefs['specialbilling_c']['value'] = $parent->specialbilling_c; };
+      if(isset($parent->name)) { $this->ev->fieldDefs['customer_c']['value'] = $parent->name; };
+    }
+    echo $this->ev->display();
+    }
+}
+?>
