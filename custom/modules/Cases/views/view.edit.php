6a1b48c77550a5ff861ca3a8afbb8f99e6467c36
aldridged <aldridged@gmail.com>
2014-05-13 02:21:10 +0000

    Case inheritance code

--- custom/modules/Cases/views/view.edit.php
+++ custom/modules/Cases/views/view.edit.php
@@ -71,6 +71,26 @@ require_once('include/EditView/EditView2.php');
 		}
 
 		$this->ev->process();
+
+		//echo "<!-- \n";
+    		//var_dump($_REQUEST);
+    		//echo "-->";
+
+		if (empty($this->ev->focus->id) && ($_REQUEST['return_relationship'] == 'projects_cases')) {
+	  		$parent = new Project();
+      			$parent->retrieve($_REQUEST['return_id']);
+	  		if((isset($parent->facility_c))&&($this->ev->fieldDefs['facility_c']['value']=="")) { $this->ev->fieldDefs['facility_c']['value'] = $parent->facility_c; };
+	  		if((isset($parent->wellname_c))&&($this->ev->fieldDefs['wellname_c']['value']=="")) { $this->ev->fieldDefs['wellname_c']['value'] = $parent->wellname_c; };
+	  		if((isset($parent->afe_c))&&($this->ev->fieldDefs['afe_c']['value']=="")) { $this->ev->fieldDefs['afe_c']['value'] = $parent->afe_c; };
+	  		if((isset($parent->po_c))&&($this->ev->fieldDefs['po_c']['value']=="")) { $this->ev->fieldDefs['po_c']['value'] = $parent->po_c; };
+	  		if((isset($parent->parish_c))&&($this->ev->fieldDefs['parish_c']['value']=="")) { $this->ev->fieldDefs['parish_c']['value'] = $parent->parish_c; };
+	  		if((isset($parent->areablock_c))&&($this->ev->fieldDefs['areablock_c']['value']=="")) { $this->ev->fieldDefs['areablock_c']['value'] = $parent->areablock_c; };
+      			if((isset($parent->specialbilling_c))&&($this->ev->fieldDefs['specialbilling_c']['value']=="")) { $this->ev->fieldDefs['specialbilling_c']['value'] = $parent->specialbilling_c; };
+	  		if((isset($parent->lat_c))&&($this->ev->fieldDefs['lat_c']['value']=="")) { $this->ev->fieldDefs['lat_c']['value'] = $parent->lat_c; };
+	  		if((isset($parent->longitude_c))&&($this->ev->fieldDefs['longitude_c']['value']=="")) { $this->ev->fieldDefs['longitude_c']['value'] = $parent->longitude_c; };
+	  		if((isset($parent->drivingdirections_c))&&($this->ev->fieldDefs['drivingdirections_c']['value']=="")) { $this->ev->fieldDefs['drivingdirections_c']['value'] = $parent->drivingdirections_c; };
+      			if((isset($parent->customer_c))&&($this->ev->fieldDefs['customer_c']['value']=="")) { $this->ev->fieldDefs['customer_c']['value'] = $parent->customer_c; };
+    		};
 		echo $this->ev->display($this->showTitle);
  	}
 
