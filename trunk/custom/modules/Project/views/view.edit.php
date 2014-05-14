86de2f28058da3afc15b0cc1471c13d400b8e90f
aldridged@gmail.com <aldridged@gmail.com@ce15a44f-6aab-047a-11eb-47c6a2e4ae69>
2014-05-14 02:36:37 +0000

    Only inherit values into a blank field in project

--- trunk/custom/modules/Project/views/view.edit.php
+++ trunk/custom/modules/Project/views/view.edit.php
@@ -15,8 +15,8 @@ class ProjectViewEdit extends ViewEdit{
     if (empty($this->ev->focus->id) && ($_REQUEST['relate_to'] == 'projects_accounts')) {
 	  $parent = new Account();
       $parent->retrieve($_REQUEST['relate_id']);
-      if(isset($parent->specialbilling_c)) { $this->ev->fieldDefs['specialbilling_c']['value'] = $parent->specialbilling_c; };
-      if(isset($parent->name)) { $this->ev->fieldDefs['customer_c']['value'] = $parent->name; };
+      if((isset($parent->specialbilling_c))&&($this->ev->fieldDefs['specialbilling_c']['value']=="")) { $this->ev->fieldDefs['specialbilling_c']['value'] = $parent->specialbilling_c; };
+      if((isset($parent->name))&&($this->ev->fieldDefs['customer_c']['value']=="")) { $this->ev->fieldDefs['customer_c']['value'] = $parent->name; };
     }
     echo $this->ev->display();
     }
