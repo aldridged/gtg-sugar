5004f233743312abb37f8607c2c1c534946cfa29
aldridged <aldridged@gmail.com>
2014-09-16 01:50:24 +0000

    Updated for correct label

--- custom/Extension/modules/Cases/Ext/Vardefs/acct_mgr_c.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/acct_mgr_c.php
@@ -2,7 +2,7 @@
 
 $dictionary["Cases"]["fields"]["acct_mgr_c"] = array (
   'name' => 'acct_mgr_c',
-  'vname' => 'LBL_ACCT_MGR_C',
+  'vname' => 'LBL_ACCT_MGR',
   'type' => 'varchar',
   'len' => '255',
   'source' => 'non-db',
