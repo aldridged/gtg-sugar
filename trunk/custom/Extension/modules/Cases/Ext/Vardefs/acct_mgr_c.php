d86e7c3911ba4bbb7cb0ca55b62f77972d811caa
aldridged@gmail.com <aldridged@gmail.com@ce15a44f-6aab-047a-11eb-47c6a2e4ae69>
2014-09-16 01:25:54 +0000

    Field to store computed account manager

---
+++ trunk/custom/Extension/modules/Cases/Ext/Vardefs/acct_mgr_c.php
@@ -0,0 +1,11 @@
+<?php
+
+$dictionary["Cases"]["fields"]["acct_mgr_c"] = array (
+  'name' => 'acct_mgr_c',
+  'vname' => 'LBL_ACCT_MGR_C',
+  'type' => 'varchar',
+  'len' => '255',
+  'source' => 'non-db',
+);
+
+?>
